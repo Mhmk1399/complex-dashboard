43c6f57f1b6e7f14be1a91dbb821a259a592ce91
mohamadhosein <mhmk1399@gmail.com>
2024-11-26 16:58:25 +0330

    Update products.ts

--- models/products.ts
+++ models/products.ts
@@ -2,8 +2,10 @@ import mongoose from "mongoose";
 
 const ProductScema = new mongoose.Schema(
   {
-    imageSrc: { type: String, required: true },
-    imageAlt: { type: String, required: true },
+    images: {
+      imageSrc: { type: String, required: true },
+      imageAlt: { type: String, required: true },
+    },
     name: { type: String, required: true },
     description: { type: String, required: true },
     category: { type: String, required: true },
@@ -13,16 +15,19 @@ const ProductScema = new mongoose.Schema(
     id: { type: String, required: true },
     innventory: { type: String, required: true },
     setting: {
+      cardBorderRadius: { type: String, required: true },
+      cardBackground: { type: String, required: true },
       imageWidth: { type: String, required: true },
-      imageHeight: { type: String, required: true },
+      imageheight: { type: String, required: true },
       imageRadius: { type: String, required: true },
-      productNameColor: { type: String, required: true },
-      productNameFontSize: { type: String, required: true },
-      productNameFontWeight: { type: String, required: true },
-      priceColor: { type: String, required: true },
-      priceFontSize: { type: String, required: true },
-      descriptionColor: { type: String, required: true },
+      nameFontSize: { type: String, required: true },
+      nameFontWeight: { type: String, required: true },
+      nameColor: { type: String, required: true },
       descriptionFontSize: { type: String, required: true },
+      descriptionFontWeight: { type: String, required: true },
+      descriptionColor: { type: String, required: true },
+      priceFontSize: { type: String, required: true },
+      pricecolor: { type: String, required: true },
       btnBackgroundColor: { type: String, required: true },
       btnTextColor: { type: String, required: true }
     }
@@ -30,4 +35,5 @@ const ProductScema = new mongoose.Schema(
   { timestamps: true }
 );
 
-export default mongoose.models.Products || mongoose.model("Products", ProductScema);
+export default mongoose.models.Products ||
+  mongoose.model("Products", ProductScema);
