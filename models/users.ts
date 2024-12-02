e37495939af35ab0ab746b9c9b0b0cde14b7fa38
mohamadhosein <mhmk1399@gmail.com>
2024-12-02 14:27:08 +0330

    auth

--- models/users.ts
+++ models/users.ts
@@ -5,8 +5,16 @@ const UserSchema = new mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    role: { type: String, enum: ['superadmin', 'vendor', 'user'], required: true, default: 'user' },
+    vendorId: { 
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: 'Vendor', 
+      required: function(this: { role: string }) { 
+        return this.role === 'user'; 
+      } 
+    },
   },
   { timestamps: true }
 );
 
- export default mongoose.models.User || mongoose.model("User", UserSchema);
+export default mongoose.models.User || mongoose.model("User", UserSchema);
