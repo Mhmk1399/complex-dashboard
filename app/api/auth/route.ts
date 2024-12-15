6a2fa9e09b295e28dbae2871572a94de50c8f9b0
mohamadhosein <mhmk1399@gmail.com>
2024-12-15 19:53:32 +0330

    delet log in in auth khali

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -3,35 +3,7 @@ import { NextResponse, NextRequest } from "next/server";
 import User from "@/models/users";
 import bcrypt from "bcryptjs";
 
-const jwt = require("jsonwebtoken");
 
-export async function login(req: NextRequest) {
-  const { email, password } = await req.json();
-
-  try {
-    await connect();
-    const user = await User.findOne({ email });
-    if (!user || !(await bcrypt.compare(password, user.password))) {
-      return NextResponse.json(
-        { message: "Invalid credentials" },
-        { status: 401 }
-      );
-    }
-
-    const token = jwt.sign(
-      { id: user._id, role: user.role, vendorId: user.vendorId || null },
-      process.env.JWT_SECRET,
-      { expiresIn: "1h" }
-    );
-
-    return NextResponse.json({ token });
-  } catch (error) {
-    return NextResponse.json(
-      { message: "Error logging in", error },
-      { status: 500 }
-    );
-  }
-}
 
 export async function POST(request: Request) {
   const { 
